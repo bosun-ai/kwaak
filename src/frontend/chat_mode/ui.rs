3c6040f00a770bd0eddd8f2299d02c843520ed77
Timon Vonk <mail@timonv.nl>
2025-01-08 12:46:00 +0100

    Fix typo

--- src/frontend/chat_mode/ui.rs
+++ src/frontend/chat_mode/ui.rs
@@ -95,7 +95,7 @@ fn render_chat_messages(f: &mut ratatui::Frame, app: &mut App, area: Rect) {
     // We need to consider the available area height to calculate how much can be shown
     //
     // Because the paragraph waps the text, we need to calculate the number of lines
-    // fromt he paragraph directly.
+    // from the paragraph directly.
     current_chat.num_lines = chat_messages.line_count(area.width);
 
     // Record the number of lines in the chat for multi line scrolling
