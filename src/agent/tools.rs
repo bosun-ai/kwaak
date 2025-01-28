7fddb6ad793a2b335595996b5225d53ad443e54c
Timon Vonk <mail@timonv.nl>
2025-01-28 14:52:18 +0100

    Ignore git in search code

--- src/agent/tools.rs
+++ src/agent/tools.rs
@@ -169,7 +169,7 @@ impl ResetFile {
     )
 )]
 pub async fn search_code(context: &dyn AgentContext, query: &str) -> Result<ToolOutput, ToolError> {
-    let cmd = Command::Shell(format!("rg -i. -F '{query}'"));
+    let cmd = Command::Shell(format!("rg -g '!.git' -i. -F '{query}'"));
     let output = accept_non_zero_exit(context.exec_cmd(&cmd).await)?;
     Ok(output.into())
 }
