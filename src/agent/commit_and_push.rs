f53381d3b790e51193cb9546a014ca717af4980e
Timon Vonk <mail@timonv.nl>
2025-02-24 12:47:53 +0100

    [kwaak]: Committed changes after completion

---
+++ src/agent/commit_and_push.rs
@@ -0,0 +1,110 @@
+use anyhow::Context as _;
+use swiftide::agents::hooks::AfterEachFn;
+use swiftide::traits::Command;
+
+use crate::{repository::Repository, util::accept_non_zero_exit};
+
+use super::env_setup::AgentEnvironment;
+
+pub struct CommitAndPush {
+    auto_commit_enabled: bool,
+    push_to_remote_enabled: bool,
+}
+
+impl CommitAndPush {
+    pub fn new(repository: &Repository, agent_env: &AgentEnvironment) -> Self {
+        let auto_commit_enabled = !repository.config().git.auto_commit_disabled;
+        let push_to_remote_enabled =
+            agent_env.remote_enabled && repository.config().git.auto_push_remote;
+
+        Self {
+            auto_commit_enabled,
+            push_to_remote_enabled,
+        }
+    }
+
+    pub fn hook(self) -> impl AfterEachFn {
+        move |agent| {
+            let auto_commit_enabled = self.auto_commit_enabled;
+            let push_to_remote_enabled = self.push_to_remote_enabled;
+
+            Box::pin(async move {
+                if auto_commit_enabled {
+                    accept_non_zero_exit(
+                        agent.context().exec_cmd(&Command::shell("git add .")).await,
+                    )
+                    .context("Could not add files to git")?;
+
+                    accept_non_zero_exit(
+                        agent
+                            .context()
+                            .exec_cmd(&Command::shell(
+                                "git commit -m \"[kwaak]: Committed changes after completion\"",
+                            ))
+                            .await,
+                    )
+                    .context("Could not commit files to git")?;
+                }
+
+                if push_to_remote_enabled {
+                    accept_non_zero_exit(
+                        agent.context().exec_cmd(&Command::shell("git push")).await,
+                    )
+                    .context("Could not push changes to git")?;
+                }
+                Ok(())
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use swiftide::agents::Agent;
+    use tokio::process::Command;
+
+    use crate::test_utils::test_repository;
+
+    use super::*;
+
+    #[test_log::test(tokio::test)]
+    async fn test_auto_commit() {
+        let (repository, _guard) = test_repository();
+
+        let commit_and_push = CommitAndPush::new(&repository, &AgentEnvironment::default());
+
+        std::fs::write(repository.path().join("test.txt"), "test").unwrap();
+
+        // noop agent
+        let llm = repository
+            .config()
+            .query_provider()
+            .get_chat_completion_model(repository.config().backoff)
+            .unwrap();
+        let mut agent = Agent::builder().llm(&llm).build().unwrap();
+
+        commit_and_push.hook()(&mut agent).await.unwrap();
+
+        // verify commit, check if the the commit message is correct and no uncommitted changes
+        let commit = Command::new("git")
+            .args(["log", "-1", "--pretty=%B"])
+            .current_dir(repository.path())
+            .output()
+            .await
+            .unwrap();
+
+        assert_eq!(
+            commit.stdout,
+            b"[kwaak]: Committed changes after completion\n"
+        );
+
+        let status = Command::new("git")
+            .args(["status", "--porcelain"])
+            .current_dir(repository.path())
+            .output()
+            .await
+            .unwrap();
+
+        assert!(status.stdout.is_empty());
+    }
+}
